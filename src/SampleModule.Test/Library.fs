5a799d8a13be628e97acddde48fa6800f3c26082
krymtkts <takatoshi.kuriyama@gmail.com>
2026-02-14 20:40:51 +0900

    Add Expecto test framework and initial test setup.

---
+++ src/SampleModule.Test/Library.fs
@@ -0,0 +1,19 @@
+﻿namespace SampleModule.Test
+
+open Expecto
+open Expecto.Flip
+
+module Core =
+    [<Tests>]
+    let tests =
+        testList
+            "Dummy tests for SampleModule.Core"
+            [
+
+              test "Dummy test" {
+
+                  1 |> Expect.equal "One should be equal to one." 1
+
+              }
+
+              ]
